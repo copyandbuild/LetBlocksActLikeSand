b3fda992c29bb05d33213e6603bf7f671b55c233
Larrox <payzitate@gmail.com>
2025-03-01 19:01:09 +0100

    Cleanup

--- src/main/java/dev/larrox/letblocksactlikesand/LetBlocksActLikeSand.java
+++ src/main/java/dev/larrox/letblocksactlikesand/LetBlocksActLikeSand.java
@@ -5,20 +5,10 @@ import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class LetBlocksActLikeSand extends JavaPlugin {
-
-//    @Override
-//    public void onLoad() {
-//        // Plugin First load logic
-//    }
-
+    
     @Override
     public void onEnable() {
         Bukkit.getPluginManager().registerEvents(new ExplosionEvent(), this);
-
     }
-
-//    @Override
-//    public void onDisable() {
-//        // Plugin shutdown logic
-//    }
+    
 }
